a914fcb93efd76189668180b8fe29b187b8b5afc
FA1F.W <fa1994f.w@gmail.com>
2023-08-07 21:22:14 +0300

    -добавил говноверсточку;

---
+++ frontend/src/components/DressCode.jsx
@@ -0,0 +1,13 @@
+const DressCode = () => (
+  <section className="dress-code">
+    <h2 className="h2">Дресс-код</h2>
+    <p className="dress-code__text">
+      Нам будет приятно, если&nbsp;Вы выберете образ, подходящий для
+      праздничного вечера и&nbsp;в&nbsp;соответствии с&nbsp;представленной ниже
+      гаммой.
+    </p>
+    <img srcSet="./img/hearts.svg" alt="правила дресс-кода" />
+  </section>
+);
+
+export default DressCode;
---
+++ frontend/src/components/Second.jsx
@@ -0,0 +1,22 @@
+const Second = () => (
+  <section className="second">
+    <p className="second__subtitle">
+      <span>Дорогие</span>
+      друзья и&nbsp;родные!!
+    </p>
+    <h1>Наша свадьба</h1>
+    <p className="second__text">
+      Один день в&nbsp;этом году будет для нас особенным и&nbsp;мы&nbsp;хотим
+      провести его в&nbsp;кругу близких и&nbsp;друзей.
+    </p>
+    <img className="rings" srcSet="./img/rings.png" alt="кольца" />
+    <h3 className="second__h3">30&nbsp;сентября 2023 года</h3>
+    <p className="second__sub-text">
+      Уделите несколько минут, чтобы узнать все тонкости мероприятия.
+    </p>
+    <h3 className="second__h3">Осталось:</h3>
+    <p className="second__timer" />
+  </section>
+);
+
+export default Second;

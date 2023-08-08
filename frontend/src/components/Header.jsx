cbc0b92608b8a78e15f9cd773c4e5ed189d7cff5
Alexey Kaledkin <hakon1@mail.ru>
2023-08-08 22:29:39 +0300

    прикрутил строку

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,23 +1,26 @@
-const Header = () => (
-  <header className="header">
-    <picture>
-      <source type="image/webp" srcSet="./img/content1.webp" alt="" />
-      <source type="image/jpg" srcSet="./img/content1.jpg" alt="" />
-      <img src="./img/content1.jpg" alt="" />
-    </picture>
-    <p className="header__content">
-      У нас&ensp;
-      <span>
-        хорошая
-        <br />
-        новость!
-      </span>
-    </p>
-    <a href="#form" className="header__twister">
-      <img srcSet="./img/heart.png" alt="" />
-      <p className="spin__text">&#173;подтвердите&#173;присутствие</p>
-    </a>
-  </header>
-);
+const Header = () => {
+  const headerSpiner = (text) => text.split('').map((letter, i) => (<span key={letter} style={{ transform: `rotate(${i * 15}deg)` }}>{letter}</span>));
+  return (
+    <header className="header">
+      <picture>
+        <source type="image/webp" srcSet="./img/content1.webp" alt="" />
+        <source type="image/jpg" srcSet="./img/content1.jpg" alt="" />
+        <img src="./img/content1.jpg" alt="" />
+      </picture>
+      <p className="header__content">
+        У нас&ensp;
+        <span>
+          хорошая
+          <br />
+          новость!
+        </span>
+      </p>
+      <a href="#form" className="header__twister">
+        <img srcSet="./img/heart.png" alt="" />
+        <p className="spin__text">{headerSpiner(' подтвердите присутствие')}</p>
+      </a>
+    </header>
+  );
+};
 
 export default Header;
--- frontend/src/utilities/index.js
+++ frontend/src/utilities/index.js
@@ -1,70 +1,68 @@
-"use strict";
 //      header's text spiner
-const headerSpiner = function () {
-    const text = document.querySelector('.spin__text');
-    text.innerHTML = text.textContent.replace(/\S/g, '<span>$&</span>');
-    const letters = document.querySelectorAll('p.spin__text span');
+const headerSpiner = () => {
+  const text = document.querySelector('.spin__text');
+  text.innerHTML = text.textContent.replace(/\S/g, '<span>$&</span>');
+  const letters = document.querySelectorAll('p.spin__text span');
 
-    for (let i = 0; i < letters.length; i++) {
-        letters[i].style.transform = "rotate(" + i * 15 + "deg)"
-    }
+  for (let i = 0; i < letters.length; i += 1) {
+    letters[i].style.transform = `rotate(${i * 15}deg)`;
+  }
 
-    /*
+  /*
     //очень интересно почему не получается преследование курсора
     document.addEventListener('mousemove', function (e) {
         text.style.left = e.clientX + 'px';
         text.style.top = e.clientY + 'px';
-    
+
         console.log(e.clientX);
         console.log(e.clientY);
     });
     */
-
-}
+};
 
 headerSpiner();
-
 
 //   TIMER
 
 const weddingDay = 'September 30 2023 11:00:00 GMT+04:00';
 
 function getTimeRemaining(endtime) {
-    let t = Date.parse(endtime) - Date.parse(new Date());
-    let seconds = Math.floor((t / 1000) % 60);
-    let minutes = Math.floor((t / 1000 / 60) % 60);
-    let hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-    let days = Math.floor(t / (1000 * 60 * 60 * 24));
-    return {
-        total: t,
-        days: days,
-        hours: hours,
-        minutes: minutes,
-        seconds: seconds
-    };
+  const t = Date.parse(endtime) - Date.parse(new Date());
+  const seconds = Math.floor((t / 1000) % 60);
+  const minutes = Math.floor((t / 1000 / 60) % 60);
+  const hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  const days = Math.floor(t / (1000 * 60 * 60 * 24));
+  return {
+    total: t,
+    days,
+    hours,
+    minutes,
+    seconds,
+  };
 }
 
 //      let;s try to optimize code
 function initializetimer(selector, endtime) {
-    let timer = document.querySelector(selector);
+  const timer = document.querySelector(selector);
 
-    const daysSpan = timer.querySelector('.days');
-    const hoursSpan = timer.querySelector('.hours');
-    const minutesSpan = timer.querySelector('.minutes');
+  const daysSpan = timer.querySelector('.days');
+  const hoursSpan = timer.querySelector('.hours');
+  const minutesSpan = timer.querySelector('.minutes');
 
-    function updatetimer() {
-        let t = getTimeRemaining(endtime);
-        daysSpan.innerHTML = t.days;
-        hoursSpan.innerHTML = t.hours;
-        minutesSpan.innerHTML = t.minutes;
+  const updatetimer = () => {
+    const t = getTimeRemaining(endtime);
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = t.hours;
+    minutesSpan.innerHTML = t.minutes;
 
-        if (t.total <= 0) {
-            clearInterval(timeInterval);
-        }
-    }
-
-    updatetimer();
     const timeInterval = setInterval(updatetimer, 1000);
+
+    if (t.total <= 0) {
+      clearInterval(timeInterval);
+    }
+  };
+
+  updatetimer();
 }
 
 initializetimer('.second__timer', weddingDay);
